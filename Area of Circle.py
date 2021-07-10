27af2108e239c7c8cf646caf3526972b76ea5740
Stifler <43244778+iamStifler@users.noreply.github.com>
2021-07-10 20:00:41 +0545

    Delete Area of Circle.py

--- Area of Circle.py
+++
@@ -1,5 +0,0 @@
-Radius=float(input("Enter Radius of Circle :"))
-Pi=3.141
-area=Pi*Radius*Radius
-circumference=2*Pi*Radius
-print("Area of Circle :" +str(area)+"\tCircumference :" +str(circumference))
